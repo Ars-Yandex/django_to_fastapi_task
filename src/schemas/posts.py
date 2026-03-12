15e2186d569c38d79433eed63a0b787c370fa009
Takagichaan <gtkimarsen2006@gmail.com>
2026-03-12 14:33:26 +0400

    Добавлены недостающие модели из Джанго проекта

--- src/schemas/posts.py
+++ src/schemas/posts.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
 from datetime import datetime
 from typing import Optional
+from src.schemas.base import BaseSchema
 from src.schemas.users import User
+from src.schemas.location import Location
+from src.schemas.category import Category
 
-class Post(BaseModel):
+class Post(BaseSchema):
     title: str = Field(..., max_length=256)
     text: str
     pub_date: datetime = Field(default_factory=datetime.now)
     is_published: bool = True
     author: User
-
-    class Config:
-        from_attributes = True
+    location: Optional[Location] = None 
+    category: Optional[Category] = None
+    image: Optional[str] = None
